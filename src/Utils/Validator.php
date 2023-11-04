84cb1878e8114a336ccf867803398ec9d73a801f
Vu Tung Lam <vutunglaminfo@gmail.com>
2023-11-04 09:30:28 +0700

    fix: rearrange validation code

--- src/Utils/Validator.php
+++ src/Utils/Validator.php
@@ -13,12 +13,12 @@ class Validator
 
     public static function validateUserUpdate_AllMustPresent(array $array, ?string $message = null): void
     {
-        /*return*/ Validator::validateArray_AllMustPresent($array, self::$specs_UserUpdate, $message);
+        /*return*/ self::validateArray_AllMustPresent($array, self::$specs_UserUpdate, $message);
     }
 
     public static function validateUserUpdate_AllAreOptional(array $array, ?string $message = null): array
     {
-        return Validator::validateArray_AllAreOptional($array, self::$specs_UserUpdate, $message);
+        return self::validateArray_AllAreOptional($array, self::$specs_UserUpdate, $message);
     }
 
     public static function validateArray_AllMustPresent(array $array, array $specs, ?string $message = null): void
@@ -50,14 +50,14 @@ class Validator
     {
         switch ($path) {
             case 'users.email':
-                self::assertNotEmpty($value, $path, $keyName, $message);
+                self::assertAsciiAndNotEmpty($value, $path, $keyName, $message);
                 if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
                     throw new ValidationException($keyName, 'is invalid', $message);
                 }
                 break;
 
             case 'users.password':
-                self::assertNotEmpty($value, $path, $keyName, $message);
+                self::assertAsciiAndNotEmpty($value, $path, $keyName, $message);
                 $N = strlen($value);
                 if ($N < 8) {
                     throw new ValidationException($keyName, 'is too short (minimum 8 characters, maximum 24 characters)', $message);
@@ -65,22 +65,22 @@ class Validator
                 else if ($N > 24) {
                     throw new ValidationException($keyName, 'is too long (minimum 8 characters, maximum 24 characters)', $message);
                 }
-                // https://stackoverflow.com/a/6497946/13680015
-                if (preg_match('/[^\x20-\x7e]/', $value)) {
-                    throw new ValidationException($keyName, 'contains invalid characters', $message);
-                }
                 break;
             
             case 'users.name':
-                self::assertNotEmpty($value, $path, $keyName, $message);
+                self::assertAsciiAndNotEmpty($value, $path, $keyName, $message);
                 break;
         }
     }
 
-    private static function assertNotEmpty(mixed &$value, string $path, string $keyName, ?string $message): void
+    private static function assertAsciiAndNotEmpty(mixed &$value, string $path, string $keyName, ?string $message): void
     {
         if (!$value) {
             throw new ValidationException($keyName, 'is empty', $message);
+        }
+        // https://stackoverflow.com/a/6497946/13680015
+        if (preg_match('/[^\x20-\x7e]/', $value)) {
+            throw new ValidationException($keyName, 'contains invalid characters', $message);
         }
     }
 }
